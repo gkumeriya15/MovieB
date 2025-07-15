22052de18e1aab35d20bae810e69bb5c6e94589b
smartwa <smartwacaleb@gmail.com>
2025-07-15 21:48:36 +0300

    feat: Add pytest mark for asynchronous testing in test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,10 +3,10 @@ from tests import session
 from moviebox_api.core import Homepage
 from moviebox_api.models import HomepageContentModel
 
+
 @pytest.mark.asyncio
 async def test_homepage():
-    """Tests homepage content fetching and modelling it
-    """
+    """Tests homepage content fetching and modelling it"""
     homepage = Homepage(session=session)
     contents = await homepage.get_content()
     assert type(contents) is dict
