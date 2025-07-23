f49b2d30ceacad087f85201138d46da4f226dd0c
Simatwa <smartwacaleb@gmail.com>
2025-07-23 16:04:20 +0300

    feat: Reconnaisance - locating tv-series download etc

--- src/test_download_movie.py
+++ src/test_download_movie.py
@@ -19,16 +19,15 @@ from moviebox_api.download import DownloadableFilesDetail, MediaFileDownloader
 
 # from moviebox_api.utils import get_filesize_string
 
-
 async def main():
     session = Session()
-    search = Search(session, "avatar", subject_type=SubjectType.MOVIES)
+    search = Search(session, "Heads of state", subject_type=SubjectType.MOVIES)
     search_results = await search.get_modelled_content()
     target_movie = search_results.items[0]
     # print("Target movie :", target_movie)
     downloadable_files = DownloadableFilesDetail(session, target_movie)
     downloadable_files_detail = await downloadable_files.get_modelled_content()
-    target_media_file = downloadable_files_detail.worst_media_file
+    target_media_file = downloadable_files_detail.best_media_file
     media_file_downloader = MediaFileDownloader(target_media_file)
     media_file_saved_to = await media_file_downloader.run(
         filename=f"{target_movie.title} - {target_media_file.resolution}p.mp4",
